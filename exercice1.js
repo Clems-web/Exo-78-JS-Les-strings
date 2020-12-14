9b9bbf912def236ba07397bd2cbbabb94f545be9
Clems-web <clement.servais1@gmail.com>
2020-12-14 09:37:26 +0100

    Finito !

--- exercice1.js
+++ exercice1.js
@@ -2,15 +2,24 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
 
+console.log(monTexte.indexOf("i"));
 
+let spanPosition = document.getElementById("position");
+spanPosition.innerHTML = monTexte.indexOf("i");
 
+let monTexteUpper = monTexte.toUpperCase();
 
+let chaineMaj = document.getElementById("chaineMaj");
+chaineMaj.innerHTML = monTexteUpper;
 
-
-
+for (x = 0; x < chaineMaj.innerHTML.length; x++) {
+    if (chaineMaj.innerHTML[x].includes("A")) {
+        alert("lettre 'A' trouvé");
+    }
+}
